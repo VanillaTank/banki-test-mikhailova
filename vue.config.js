fce3c647b71cd8b56a6e97ffd2d729da2f7784d0
tanya <tatiana.mikhailova.dev@gmail.com>
2026-08-05 18:08:38 +0400

    добавлен обработчик для svg

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
@@ -7,5 +8,15 @@ module.exports = defineConfig({
         additionalData: `@import "@/assets/styles/variables.scss";`
       }
     }
+  },
+  chainWebpack: (config) => {
+    config.module.rules.delete('svg');
+    // правило: обрабатывать SVG через vue-svg-loader
+    config.module
+      .rule('svg-component')
+      .test(/\.svg$/)
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .end();
   },
 })
