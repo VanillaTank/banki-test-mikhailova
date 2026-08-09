d25a932d62a3aa62671368ac1fa9840319bc78b3
tanya <tatiana.mikhailova.dev@gmail.com>
2026-08-10 02:30:55 +0400

    оптимизация сборки: вес файлов шрифтов

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,10 @@ module.exports = defineConfig({
     },
   },
   chainWebpack: (config) => {
+    // увеличить рекомендуемый размер файла ассета до самого большого файла шрифта
+    const maxFontWeight = 380_000
+    config.performance.maxAssetSize(maxFontWeight)
+
     config.module.rules.delete('svg')
     // правило: обрабатывать SVG через vue-svg-loader
     config.module
